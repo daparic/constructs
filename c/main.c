b1f430a8c3b36b5ee90c9f73996f0e5865f78e96
Your Name <you@example.com>
2023-03-01 12:46:53 +0800

    Use _Generic

--- c/main.c
+++ c/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+/*
+8.23 Generic selections
+
+Rule 23.6
+The controlling expression of a generic selection shall have an essential type
+that matches its standard type
+*/
+
+#define TYPE_NAME(X)                                                           \
+    _Generic((X), char                                                         \
+             : "Character", int                                                \
+             : "Integer", double                                               \
+             : "Double", default                                               \
+             : "Unknown")
+
 int main(int argc, char *argv[]) {
+    printf("Type Name: %s\n",
+           TYPE_NAME('4')); // Checks the type of the given input
     return 0;
 }
