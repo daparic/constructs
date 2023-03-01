b5da0878344cd37b4d67ca54205b2ccc596d3a5f
Your Name <you@example.com>
2023-03-01 12:05:15 +0800

    add c

---
+++ c/misra.c
@@ -0,0 +1,18 @@
+#include <stdnoreturn.h>
+#include <stdalign.h>
+
+int addnum(int a, int b) {
+    int result =  a + b;
+    if (result == 0) {
+        result = -1;
+    }
+    return result;
+}
+
+_Noreturn void f(int a, int b) {
+    while (1) {
+        if (addnum(a,b) == 0) {
+            break;
+        }
+    }
+}
