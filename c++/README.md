c3b9f8b152ed48efbbdedd7e15d652435cb05ef2
Your Name <you@example.com>
2023-03-15 13:04:34 +0800

    Improve readme on memory leak checking

--- c++/README.md
+++ c++/README.md
@@ -1,3 +1,4 @@
+## Build
 ```
 mkdir build && build
 cmake ..
@@ -9,3 +10,18 @@ Test your code:
 ./main
 ctest --verbose
 ```
+
+## Memory Leak Check 
+
+Setup alias by:
+```
+alias valgrind='valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt'
+```
+
+Then run your specific test executable code:
+```
+valgrind build/tests/main_test
+valgrind build/main
+```
+
+Then examined generated file `valgrind-out.txt`
--- c++/tests/main_test.cpp
+++ c++/tests/main_test.cpp
@@ -1,10 +1,34 @@
 #include <gtest/gtest.h>
+#include <iostream>
+#include <vector>
 
-TEST(MultiplyTests, TestIntegerOne_One) {
+class Ubject {
+public:
+    Ubject() {
+        std::cout << "[Ubject]" << std::endl;
+    }
+    ~Ubject() {
+        std::cout << "[~Ubject]" << std::endl;
+    }
+};
+
+TEST(BasicTest, AddingNumbers) {
     auto addnum = [](int a, int b) { return a + b; };
     const auto expected = 69;
     const auto actual = addnum(42, 13) + 14;
     ASSERT_EQ(expected, actual);
+}
+
+TEST(MemoryLeak, Leak01) {
+    auto func = []() {
+        std::vector<Ubject *> accounts;
+        for (int i = 0; i < 9; i++) {
+            Ubject *pUbject = new Ubject;
+            accounts.push_back(pUbject);
+        }
+    };
+
+    func();
 }
 
 int main(int argc, char **argv) {
